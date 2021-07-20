0bfdfafc786e3092a4eee6f06377d89c2ed2d3f0
Tatsuki Yamantani <tatsuki.y@wsad.io>
2021-07-19 21:15:04 -0700

    Change setting

--- index.js
+++ index.js
@@ -12,16 +12,14 @@ app.use(morgan("combined"));
 
 app.use(bodyParser.json({ type: "*/*" }));
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Max-Age", "86400");
-  next();
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
 app.use(
