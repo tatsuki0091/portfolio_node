1219713496abb1ead6a610f3c32f4ba933b6a743
Tatsuki Yamantani <tatsuki.y@wsad.io>
2021-07-19 20:59:53 -0700

    Change setting

--- index.js
+++ index.js
@@ -16,6 +16,25 @@ app.use(
   })
 );
 app.use(bodyParser.json({ type: "*/*" }));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Max-Age", "86400");
+  next();
+});
+
+/**
+ \* OPTIONSメソッドの実装
+ \*/
+app.options("*", function (req, res) {
+  res.sendStatus(200);
+});
 email(app);
 
 // Serverの設定
